4a20d397e261d889da4af1c09ccaad047d9c1cde
josemar <estudos.josemarbrito@gmail.com>
2020-11-19 23:14:03 -0300

    Configurado lib Python-Decouple close #19

--- jbs/settings.py
+++ jbs/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'h=l=v7n6u+sctuzge!0t==bk@mwkq#j)21slg2cdhv6k-y!4ya'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
