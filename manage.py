425cd3a83fc3510c219186b8f054cc6b3158f4e8
josemar <estudos.josemarbrito@gmail.com>
2020-12-01 22:12:12 -0300

    Configurado customização de banco de dados e migrações

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+if __name__ == '__main__':
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jbs.settings')
     try:
@@ -16,7 +16,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
